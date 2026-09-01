101fd32b94aac97f069bccb2a628c0b82af07d45
httpfabiana <fabiana.sylva99@gmail.com>
2026-08-31 23:33:03 -0300

    feat: I added image generation pages.

--- client/src/pages/GenerateImages/GenerateImages.jsx
+++ client/src/pages/GenerateImages/GenerateImages.jsx
@@ -1,12 +1,95 @@
 
-
-import React from 'react'
+import { Edit, Hash, Sparkles, Image } from 'lucide-react'
+import React from 'react';
+import { useState } from 'react'
 
 const GenerateImages = () => {
+
+  const ImagesStyle = ['Realistic', 'Ghibli style', 'Anime style', 'Cartoon style', 
+    'Fantasy style', 'Realistic', '3D style', 'Portrait style']
+   
+   const [selectedStyle, setSelectedStyle] = useState('Realistic')
+   
+   const [input, setInput] = useState('');
+
+   const [publish, setPublish] = useState(false)
+   
+   const onSubmitHandler = async(e) => {
+    e.preventDafault();
+   }
+
   return (
-    <div>
-      
-    </div>
+     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+          <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+           <div className='flex items-center gap-3'>
+            <Sparkles className='w-6 text-[#00Ad25]'/>
+            <h1 className='text-xl font-semibold'>Gerador de Imagens por IA</h1>
+           </div>
+    
+           <p className='mt-6 text-sm font-medium'>
+             Descreva sua Image
+           </p>
+           <textarea
+            type='text'
+            placeholder='Descreva o que voce quer ver na image...'
+            className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
+            rows={4}
+            required
+            value={input}
+            onChange={(e)=> setInput(e.target.value)}
+           />
+           <p className='mt-4 text-sm font-medium'>
+             Style
+           </p>
+           <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+            {ImagesStyle.map((item) => (
+              <span onClick={() => setSelectedStyle(item)} key={item} className={`text-xs px-4 py-1 border rounded-full cursor-pointer 
+              ${selectedStyle === item ? 'bg-green-50 text-green-700' : 'text-gray-500 border-gray-300'}`}>
+                {item}
+              </span>
+            ))}
+           </div>
+
+           <div className='my-6 flex items-center gap-'>
+            <label className='relative cursor-pointer'>
+              <input 
+               type='checkbox'
+               onChange={(e)=> setPublish(e.target.checked)}
+               checked={publish}
+               className='sr-only peer'
+               />
+
+               <div className='w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition'></div>
+
+               <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4'></span>
+            </label>
+             <p className='text-sm ml-2'>Tornar esta imagem pública</p>
+           </div>
+
+           <br/>
+           <button className='w-full flex justify-center items-center gap-2 from-[#00ad25] bg-[#00ad25] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+            <Image className='w-5'/>
+             Gerar Image
+           </button>
+          </form>
+    
+          {/* */}
+         <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+          <div className='flex items-center gap-3'>
+            <Image className='w-5 h-5 text-[#00ad25]'/>
+            <h1 className='text-xl font-semibold'>Gerado image</h1>
+          </div>
+    
+          <div className='flex-1 flex justify-center items-center'>
+           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+            <Image className='w-9 h-9'/>
+            <p>
+              Insira um tópico e clique em "Gerar image" para começar.
+            </p>
+           </div>
+          </div>
+         </div>
+        </div>
   )
 }
 
