447313aad18ac71862a5b42960fa2196c587e21b
httpfabiana <fabiana.sylva99@gmail.com>
2026-09-01 22:02:32 -0300

    feat: add backedn

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import {clerkMiddleware} from '@clerk/express'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get('/', (req, res) => res.send('servidor rodando'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Servidor rodado na porta', PORT);
+})
